a656734245c7598699ce0fa014f6b22a4ec3fefc
dstrandell <dstrande@live.ca>
2024-04-10 09:50:46 -0700

    Add plots and clean code

--- clean_plot.py
+++ clean_plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta, date
 import numpy as np
 import h5py
-from os import path
+import plotly.graph_objects as go
 
 
 def round_seconds(obj: datetime) -> datetime:
@@ -16,73 +16,99 @@ gc = gspread.service_account()
 
 rooms = ["TV Room", "Living Room", "Laundry", "Bedroom"]
 
-for i in range(2):  # Change to 4 once logging on all 4 starts.
+fig = go.Figure()
+
+for i in range(4):
     print(rooms[i])
     worksheet = gc.open(f"esp32 logging {i + 1}").sheet1
     times = worksheet.col_values(1)
     temperatures = worksheet.col_values(2)
     humidities = worksheet.col_values(3)
+    sheets_length = len(times)
 
     # Find last pulled index
     cell = worksheet.find("Last Pulled")
     if cell:
         print(cell)
+    else:
+        cell = 0
 
     # Create dataframe
     data_tv = pd.DataFrame(list(zip(times, temperatures, humidities)))
     data_tv.columns = ["Time", "Temperature (C)", "Humidity (%)"]
-    data_tv = data_tv[data_tv['Temperature (C)'] != "0"]
+    data_tv = data_tv[data_tv["Temperature (C)"] != "0"]
     data_tv.reset_index(inplace=True, drop=True)
-    # print(len(data_tv))
-    # print(data_tv.head(25))
 
     # Calcuate datetimes
     length = len(data_tv["Humidity (%)"])
-    dates = data_tv['Time']
+    dates = data_tv["Time"]
     dates = dates[dates.str.contains(":").fillna(False)]
 
-    for i in range(len(dates)):
-        date_split = dates.iat[i].split(" ")
+    for k in range(len(dates)):
+        date_split = dates.iat[k].split(" ")
         datet = f"{date_split[1]} {date_split[2]}"
         datet = datetime.strptime(datet, "%Y%m%d %H:%M:%S")
-        if i == 0:
+        if k == 0:
             print(datet)
             time0 = datet
-            j = dates.index[i]
+            j = dates.index[k]
 
-        elif i == len(dates) - 1:
+        elif k == len(dates) - 1:
             print(datet)
             deltat = datet - time0
-            delta_idx = dates.index[i] - j
+            delta_idx = dates.index[k] - j
 
     data_tv["Time"] = pd.date_range(
-        start=(datet - (length * deltat / delta_idx)),
-        end=datet,
-        periods=length
+        start=(datet - (length * deltat / delta_idx)), end=datet, periods=length
     )
 
-    # Convert to floats for h5
+    # Convert to data to floats for h5
     time_array = data_tv["Time"].to_numpy()
     time_array = time_array.astype(float)
     time_array.shape
-    data_array = data_tv[['Temperature (C)', 'Humidity (%)']].to_numpy()
+    data_array = data_tv[["Temperature (C)", "Humidity (%)"]].to_numpy()
     data_array = data_array.astype(float)
     data_array.shape
-    tv_room = np.concatenate((time_array[:, None], data_array), axis=1)
-    # Convert time back to datetime
-    # time_array = pd.to_datetime(time_array, unit='ns')
+    data_array = np.concatenate((time_array[:, None], data_array), axis=1)
 
     # Create dataset if it doesn't exist, append if it does
-    with h5py.File('data/temperature_data.h5', 'a') as f:
+    with h5py.File("data/temperature_data.h5", "w") as f:
         try:
-            f.create_dataset(f"{rooms[i]}", data=tv_room, compression="gzip", chunks=True, maxshape=(None, 4))
+            f.create_dataset(
+                rooms[i],
+                data=data_array,
+                compression="gzip",
+                chunks=True,
+                maxshape=(None, 4),
+            )
         except ValueError:
+            # Overwrite data until gsheets data is deleted
+            f[rooms[i]] = data_array
+
             # Append new data to it
-            f[f"{rooms[i]}"].resize((f[f"{rooms[i]}"].shape[0] + tv_room.shape[0]), axis=0)
-            f[f"{rooms[i]}"][-tv_room.shape[0]:] = tv_room
-    # f = h5py.File('data/temperature_data.h5', 'a')
-    # f.close()
+            # f[rooms[i]].resize((f[rooms[i]].shape[0] + tv_room.shape[0]),
+            #                     axis=0,)
+            # f[rooms[i]][-tv_room.shape[0]:] = tv_room
 
-    worksheet.update_acell(f'D{length + cell}', f"Last Pulled on {date.today().isoformat()}")
+    worksheet.update_acell(
+        f"D{sheets_length + cell}", f"Last Pulled on {date.today().isoformat()}"
+    )
 
-    # TODO: Plotly html plots, create batch script, Correct for TZ+DLS
+    with h5py.File("data/temperature_data.h5", "r") as f:
+        all_data = pd.DataFrame(np.array(f[rooms[i]]))
+
+    all_data.columns = ["Time", "Temperature (C)", "Humidity (%)"]
+    all_data["Time"] = pd.to_datetime(all_data["Time"], unit="ns", utc=True)
+    all_data["Time"] = all_data["Time"].dt.tz_convert("US/Pacific")
+
+    fig.add_trace(
+        go.Scatter(
+            x=all_data["Time"],
+            y=all_data["Temperature (C)"],
+            mode="lines",
+            name=rooms[i],
+        )
+    )
+
+fig.write_html("plots/plot.html")
+# TODO: create batch script to automate, delete gsheets data after saving to h5
